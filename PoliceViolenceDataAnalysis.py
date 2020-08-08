f2ba8fb225d0b9626aef23aa32f01d1fb6c92eec
Chasity Savella <csavella@pdx.edu>
2020-08-08 13:25:38 -0700

    graph num killings over time for states with highest disparity

--- PoliceViolenceDataAnalysis.py
+++ PoliceViolenceDataAnalysis.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
+import plotly.io as pio
 import iplot
+import states_dict
 
 police_killings_df = pd.read_csv('PoliceKillings.csv')
 killings_by_state_df = pd.read_csv('KillingsByState.csv')
+individual_victims_df = pd.read_csv('IndividualVictims.csv')
 
 black_population = killings_by_state_df.AfricanAmericanAlone
 black_ppl_killed = killings_by_state_df.BlackPeopleKilled
@@ -23,12 +26,40 @@ for i,j in zip(white_population, white_ppl_killed):
 for i,j in zip(black_killed_per_population, white_killed_per_population):
     ratio_black_to_white_killed.append(i/j)
 
+highest_disparity_states = ['AK', 'MA', 'NE', 'NJ', 'IL', 'UT', 'DC', 'RI']
+
 
 
 #Police killings over time
-#***WIP***
-fig = px.line(police_killings_df, x = 'DateofIncident', y = 'Count', title='Police Killings over time (2013-2020)')
-fig.show()
+opt = []
+opts = []
+for i in range(0, len(highest_disparity_states)):
+    opt = dict(
+        target = individual_victims_df['State'][[i]].unique(), value = dict(marker = dict(color = highest_disparity_states[i]))
+    )
+    opts.append(opt)
+
+data = [dict(name = 'Alaska', type = 'line', x = individual_victims_df.DateOfIncident, y = individual_victims_df.Count, mode = 'lines', text = individual_victims_df.State, hoverinfo = 'y+text',
+			    transforms = [dict(type = 'filter', target = individual_victims_df['State'], operation = '=', value = 'AK')]),
+		dict(name = 'Massachusetts', type = 'line', x = individual_victims_df.DateOfIncident, y = individual_victims_df.Count, mode = 'lines', text = individual_victims_df.State, hoverinfo = 'y+text',
+				transforms = [dict(type = 'filter', target = individual_victims_df['State'], operation = '=', value = 'MA')]),
+		dict(name = 'Nebraska', type = 'line', x = individual_victims_df.DateOfIncident, y = individual_victims_df.Count, mode = 'lines', text = individual_victims_df.State, hoverinfo = 'y+text',
+				transforms = [dict(type = 'filter', target = individual_victims_df['State'], operation = '=', value = 'NE')]),
+		dict(name = 'New Jersey', type = 'line', x = individual_victims_df.DateOfIncident, y = individual_victims_df.Count, mode = 'lines', text = individual_victims_df.State, hoverinfo = 'y+text',
+				transforms = [dict(type = 'filter', target = individual_victims_df['State'], operation = '=', value = 'NJ')]),
+		dict(name = 'Illinois', type = 'line', x = individual_victims_df.DateOfIncident, y = individual_victims_df.Count, mode = 'lines', text = individual_victims_df.State, hoverinfo = 'y+text',
+				transforms = [dict(type = 'filter', target = individual_victims_df['State'], operation = '=', value = 'IL')]),
+		dict(name = 'Utah', type = 'line', x = individual_victims_df.DateOfIncident, y = individual_victims_df.Count, mode = 'lines', text = individual_victims_df.State, hoverinfo = 'y+text',
+				transforms = [dict(type = 'filter', target = individual_victims_df['State'], operation = '=', value = 'UT')]),
+		dict(name = 'District of Columbia', type = 'line', x = individual_victims_df.DateOfIncident, y = individual_victims_df.Count, mode = 'lines', text = individual_victims_df.State, hoverinfo = 'y+text',
+				transforms = [dict(type = 'filter', target = individual_victims_df['State'], operation = '=', value = 'DC')]),
+		dict(name = 'Rhode Island', type = 'line', x = individual_victims_df.DateOfIncident, y = individual_victims_df.Count, mode = 'lines', text = individual_victims_df.State, hoverinfo = 'y+text',
+				transforms = [dict(type = 'filter', target = individual_victims_df['State'], operation = '=', value = 'RI')])				
+]
+
+layout = dict(title = 'Killings over time')
+fig_dict = dict(data=data, layout=layout)
+pio.show(fig_dict, validate=False)
 
 
 
